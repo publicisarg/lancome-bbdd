2102e71ec54f56f07707f0e97652238b37c2677f
Ivo2313 <ivo.estevan@gmail.com>
2023-04-17 18:50:04 -0300

    portada

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 
 export const Portada = () => {
   return (
-    <div>
-      PORTADA
-    </div>
+    <header>
+      <div className='bg-black py-3 flex justify-center items-center'>
+        <img alt='Logo' src='/../public/img/Landing-Lancome.svg' className='w-60'/>
+      </div>
+      <div className='bg-[#FFAFBC] justify-center '>
+      <img alt='portada' src='/../public/img/portada.png' className='w-full'/>
+      </div>
+    </header>
   )
 }
 
 export const Title = () => {
   return (
-    <div className='bg-[#FFAFBC] py-9'>
-       TEXTO
-    </div>
+    <section className='bg-[#FFAFBC] py-9 justify-center text-center'>
+       <h3 className='font-quentin text-5xl'>Bonjour</h3>
+       <p className='text-5xl'>¡QUEREMOS CONOCERTE MEJOR!</p>
+    </section>
   )
 }
