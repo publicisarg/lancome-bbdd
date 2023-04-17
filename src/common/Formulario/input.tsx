dfd37569095346333ea2b1a00d63eed9ed08ce4a
jumperning <maxy.m.rodriguez@gmail.com>
2023-04-17 17:09:03 -0300

    initial 2

---
+++ src/common/Formulario/input.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Input = () => {
+  return (
+    <div>Input</div>
+  )
+}
+export const Checkbox = () => {
+  return (
+    <div>Checkbox</div>
+  )
+}
---
+++ src/common/subtitle.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Subtitle = (titulo) => {
+  return (
+    <p>{titulo}</p>
+  )
+}
