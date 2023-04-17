7a40b7f83e30cb1e0131622f322fa661a8fbb244
Ivo2313 <ivo.estevan@gmail.com>
2023-04-17 17:10:07 -0300

    hello word

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+
+    fontFamily:{
+
+       'quentin':['quentin', 'sans-serif']
+      
+       },
+       
+    extend: {
+      
+    },
   },
   plugins: [],
 }
