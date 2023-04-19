5fce883b4789a23260cddcc537cfabf7ee2ab0d7
Emanuel Musacco <emamusac@publicisgroupe.net>
2023-04-19 17:17:49 -0300

    responsive

--- src/common/Formulario/contenido-form/index.tsx
+++ src/common/Formulario/contenido-form/index.tsx
@@ -4,13 +4,13 @@ import { Input, Checkbox } from '../input'
 export const DatosPersonales = () => {
     return (
         <>
-        <div className='flex'>
+        <div className='md:flex'>
             <div className='flex flex-col w-1/2 my-4 mr-4'>
                 <Input name="nombre" placeholder="Nombre*" type="text" />
                 <Input name="fechanac" placeholder="FECHA DE NAC.: dd/mm/aaaa*" type="text" />
                 <Input name="email" placeholder="EMAIL*" type="text" />
             </div>
-            <div className='flex flex-col  w-1/2 my-4 ml-4'>
+            <div className='flex flex-col  w-1/2 md:my-4 md:ml-4'>
                 <Input name="apellido" placeholder="Apellido*" type="text" />
                 <Input name="email" placeholder="EMAIL*" type="text" />
                 <Input name="localidad" placeholder="LOCALIDAD" type="text" />
@@ -52,14 +52,14 @@ export const QuisieraSaber = () => {
 export const Preocupacion = () => {
     return (
         <>
-        <div className='flex'>
-            <div className='flex flex-col w-full my-4'>
+        <div className='md:flex'>
+            <div className='flex flex-col w-full md:my-4 mt-4'>
                 <Checkbox name="Deshidratacion" label="Deshidratacion"  />
                 <Checkbox name="Uniformidad del tono" label="Uniformidad del tono"  />
                 <Checkbox name="Líneas finas Arrugas profundas" label="Líneas finas / Arrugas profundas" />
                 <Checkbox name="Textura" label="Textura" />
             </div>
-            <div className='flex flex-col w-full my-4'>
+            <div className='flex flex-col w-full md:my-4 mb-4'>
                 <Checkbox name="Acné" label="Acné"  />
                 <Checkbox name="Falta de luminosidad" label="Falta de luminosidad"  />
                 <Checkbox name="Pérdida de firmeza" label="Pérdida de firmeza" />
@@ -72,12 +72,12 @@ export const Preocupacion = () => {
 export const Buscando = () => {
     return (
         <>
-        <div className='flex'>
-            <div className='flex flex-col w-full my-4'>
+        <div className='md:flex'>
+            <div className='flex flex-col w-full md:my-4 mt-4'>
             <Checkbox name="Descuentos y beneficios" label="Descuentos y beneficios"  />
             <Checkbox name="Aprender a usar los productos" label="Aprender a usar los productos"  />
             </div>
-            <div className='flex flex-col w-full my-4'>
+            <div className='flex flex-col w-full md:my-4'>
             <Checkbox name="Descubrir buenas reseñas" label="Descubrir buenas reseñas"  />
             <Checkbox name="AVolver a comprar" label="Volver a comprar"  />
             </div>
@@ -89,6 +89,6 @@ export const Buscando = () => {
 
 export function Boton (props: any) {
     return (
-       <button className='py-2 px-9 bg-black hover:bg-[#FFAFBC] text-white font-bold'>{props.name}</button>
+       <button className='md:py-2 py-2 md:px-9 px-16 md:text-base text-[22px] mb-14 bg-black hover:bg-[#FFAFBC] text-white font-bold'>{props.name}</button>
     )
   }
